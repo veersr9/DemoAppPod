bd22d7ad9a70bec181078d38270647b07a2c6ea3
Veerendra Pratap Singh <veersr9@gmail.com>
2022-07-12 19:34:38 +0530

    new changes

---
+++ DemoAppPod/mainFile.swift
@@ -0,0 +1,29 @@
+//
+//  mainFile.swift
+//  DemoAppPod
+//
+//  Created by veerendra Pratap Singh on 12/07/22.
+//
+
+import UIKit
+
+class mainFile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
